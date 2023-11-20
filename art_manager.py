c090af3f8edeb5e55e3acae6bec2b333a79209e3
ikuma <chemicalbondo@gmail.com>
2023-11-20 17:08:21 +0900

    Initial commit

---
+++ art_manager.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).absolute().parent
+MONSTER_ART_DIR = BASE_DIR / "ascii_art" / "monsters"
+TEXT_ART_DIR = BASE_DIR / "ascii_art" / "text"
+
+
+def get_art(file_name: str, dir_path: Path) -> str:
+    """
+    テキストアスキーアートを返す
+    :param dir_path: 対象ディレクトリ
+    :param file_name: アスキーアートのファイル名
+    :return: アスキーアート
+    """
+
+    with Path.open(dir_path / file_name, "r", encoding="utf-8") as f:
+        return f.read()
